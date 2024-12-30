5c51653a44462865c5531b9136886bc325517399
Jaeho Cha <ischar@naver.com>
2024-12-30 11:23:09 +0900

    style(navbar): adjust spacing and font size in navigation bar

--- app/resources/views/layouts/navigation.blade.php
+++ app/resources/views/layouts/navigation.blade.php
@@ -1,24 +1,24 @@
 <nav x-data="{ open: false }" class="bg-light-bg-primary dark:bg-dark-bg-primary text-light-fg-primary dark:text-dark-fg-primary border-b-1 dark:border-dark-bg-border">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex justify-between h-16 " >
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <h2 class="text-light-fg-primary dark:text-dark-fg-primary font-semibold">Chartly</h2>
+                        <h2 class="text-xl text-light-fg-primary dark:text-dark-fg-primary font-semibold">Chartly</h2>
                     </a>
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="hidden sm:flex sm:items-center sm:ms-6 space-x-8">
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('portfolio')" :active="request()->routeIs('portfolio')">
+                    <x-nav-link :href="route('portfolio')" :active="request()->routeIs('portfolio')" class="text-lg text-light-fg-primary dark:text-dark-fg-primary">
                         {{ __('Portfolio') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('watchlist')" :active="request()->routeIs('watchlist')">
+                    <x-nav-link :href="route('watchlist')" :active="request()->routeIs('watchlist')" class="text-lg text-light-fg-primary dark:text-dark-fg-primary" >
                         {{ __('Watchlist') }}
                     </x-nav-link>
                 </div>
